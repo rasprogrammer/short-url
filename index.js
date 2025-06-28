7f867c6f4ca0a8e63adc01a7bb854dde1df92a5e
rasprogrammer <rasprogrammer@gmail.com>
2025-06-28 08:51:42 +0530

    adding static view page design for short-url

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
@@ -6,6 +7,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, 'views'));
+
+app.use("/css", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")));
+app.use("/js", express.static(path.join(__dirname, "node_modules/bootstrap/dist/js")));
 
 const mongoDbConnect = require("./connection");
 
@@ -14,6 +19,14 @@ mongoDbConnect("mongodb://127.0.0.1:27017/short-url");
 const urlRouter = require("./routes/urlRouter");
 
 app.use('/url', urlRouter);
+
+app.use('/', (req, res) => {
+    let people = ['geddy', 'neil', 'alex'];
+
+    res.render("homepage", {
+        people,
+    });
+});
 
 
 app.listen(port, () => console.log('Server started!'));
