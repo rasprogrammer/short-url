e4606787872b03856bef485ee918dde7ae7d4527
Rajeev Kumar <rasprogrammer@gmail.com>
2026-04-11 16:40:43 +0530

    user model schema added

--- backend/src/models/userModel.ts
+++ backend/src/models/userModel.ts
@@ -1,11 +1,33 @@
 
-/*
-    id : auto increment primarykey
-    name
-    email
-    password_hash
-    plan ('free', 'pro', 'enterprise') default 'free'
-    is_active 
-    created_at 
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
 
-*/
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    }, 
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }, 
+    password_hash: {
+        type: String,
+    },
+    plan: {
+        type: String,
+        enum: ['free', 'pro', 'enterprise'],
+        default: 'free'
+    }, 
+    is_active: {
+        type: Boolean,
+        default: true,
+    },
+}, {
+    timestamps: true,
+});
+
+const User = model('User', userSchema);
+
+export default User;
